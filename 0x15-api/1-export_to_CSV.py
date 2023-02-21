f4a6a6646c63f835c853682568556d1b35664a8f
AlhassanMumin <alhassanmumin8@gmail.com>
2023-02-21 04:56:37 -0800

    export to csvc file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given 
-    employee ID to CSV format.
-"""
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
 
 if __name__ == "__main__":
-    ID = sys.argv[1]
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + 'users/{}'.format(ID)).json()
-    user_name = user.get('username')
-    todo = requests.get(url + 'todos', auth=('user', 'pass')).json()
-    with open('{}.csv'.format(ID), 'w', newline='') as myfile:
-         writer = csv.writer(myfile,quoting=csv.QUOTE_ALL)
-         [writer.writerow(
-            [ID, user_name, t.get("completed"), t.get("title")]
-         ) for t in todo]
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
