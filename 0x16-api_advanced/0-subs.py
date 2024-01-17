9eee4eca7f598cd650187d3803659676a7bbc74d
Mumin8 <alhassanmumin8@gmail.com>
2024-01-17 15:35:41 +0000

    subscriber count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,21 +2,15 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-def number_of_subscribers(subreddit):
-    """
-    Return the total number of subscribers on a given subreddit.
 
-    :param subreddit: The name of the subreddit.
-    :return: The total number of subscribers.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
     if response.status_code == 404:
         return 0
-    
     results = response.json().get("data")
     return results.get("subscribers")
