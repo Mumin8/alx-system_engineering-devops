133705303532887e40151bc44d482070013effe9
Mumin8 <alhassanmumin8@gmail.com>
2024-01-17 16:05:20 +0000

    subscriber count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
-
+"""Function to query reddit api"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """Return the total number of subscribers on a given subreddit."""
+    if subreddit is None:
+        return None
+    my_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16advancedapi:v1.0.0 (by /u/Mumin_8)"
+    }
+    resp = requests.get(my_url, headers=headers, allow_redirects=False)
+    if resp.status_code == 404:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    results = resp.json().get("data")
+    return results.get("subscribers")
