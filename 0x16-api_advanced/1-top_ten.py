fe67037a0e84daf8a7919e1b45acbeb1afae9746
Mumin8 <alhassanmumin8@gmail.com>
2024-01-18 09:34:57 +0000

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Function to query Reddit API"""
+"""Function to query reddit api"""
 import requests
-import sys
+
 
 def top_ten(subreddit):
-    """Return the titles of the top 10 posts from a given subreddit."""
+    """Return the total number of subscribers on a given subreddit."""
 
     my_url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
 
@@ -12,19 +12,11 @@ def top_ten(subreddit):
         "User-Agent": "linux:0x16advancedapi:v1.0.0 (by /u/Mumin_8)"
     }
 
-   
     resp = requests.get(my_url, headers=headers, allow_redirects=False)
-    resp.raise_for_status()
-
-    try:
-        some_Obj = resp.json()
-        print(some_Obj)
-        if some_Obj == '':
-            print(f"OK")
-            return
-
-        results = some_Obj['data'].get('children', [])
+    if resp.status_code == 200:
+        results = resp.json().get("data")
+        results = results.get('children')
         for result in results:
             print(result['data']['title'])
-    except requests.exceptions.JSONDecodeError as err:
-        print("None")
+    else:
+        print('None')
