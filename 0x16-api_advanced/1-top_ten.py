6681eb428c2806e9688b71afeee561c705f36cb5
Mumin8 <alhassanmumin8@gmail.com>
2024-01-18 08:17:59 +0000

    top_ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,20 +2,25 @@
 """Function to query reddit api"""
 import requests
 
+
 def top_ten(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    
+
     my_url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
 
     headers = {
         "User-Agent": "linux:0x16advancedapi:v1.0.0 (by /u/Mumin_8)"
     }
-   
+
     resp = requests.get(my_url, headers=headers, allow_redirects=False)
     if resp.status_code == 404:
         print(None)
         return
-    results = resp.json().get("data")
-    results = results.get('children')
-    for result in results:
+    some_Obj = resp.json()
+    if some_Obj is not None:
+        results = resp.json().get("data")
+        results = results.get('children')
+        for result in results:
         print(result['data']['title'])
+    else:
+        pass
