a19c9a91f86f3bd0704dcc636df865ac8a377f4b
Mumin8 <alhassanmumin8@gmail.com>
2024-01-18 10:49:14 +0000

    subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""
+queries the Reddit API and returns the number of subscribers
+"""
+import json
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "emanuelmaimona"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """get request to obtain subscribers"""
+    headers = {'User-Agent': 'linux:com.myapp:v.1'}
+    response = requests.get('https://www.reddit.com/r/{}/about'.
+                            format(subreddit), headers=headers)
+
     if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+                return 0
+
+    n_response = requests.get('https://www.reddit.com/r/{}/about.json'.
+                              format(subreddit), headers=headers).json()
+
+    return n_response.get('data').get('subscribers')
